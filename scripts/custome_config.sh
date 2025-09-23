165f822993f77515cbd512b80774f676bf775d4b
BlueG1ue <147564423+BlueG1ue@users.noreply.github.com>
2025-09-23 14:42:03 +0300

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,3 +3,7 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+
+echo "CONFIG_PACKAGE_amneziawg-tools=y" >> configs/rockchip/01-nanopi
+echo "CONFIG_PACKAGE_kmod-amneziawg=y" >> configs/rockchip/01-nanopi
+echo "CONFIG_PACKAGE_luci-app-amneziawg=y" >> configs/rockchip/01-nanopi
